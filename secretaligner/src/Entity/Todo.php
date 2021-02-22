a990d0ee3831ba77cee5922341c95ff58646ad14
Ovidiu Muresan <osilvium@gmail.com>
2021-02-22 18:52:40 +0100

    Database and Entity:Todo

---
+++ secretaligner/src/Entity/Todo.php
@@ -0,0 +1,101 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\TodoRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass=TodoRepository::class)
+ * @ORM\HasLifecycleCallbacks
+ */
+class Todo
+{
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $nombre;
+
+    /**
+     * @ORM\Column(type="datetime")
+     */
+    private $fecha_creacion;
+
+    /**
+     * @ORM\Column(type="datetime")
+     */
+    private $fecha_tope;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $estado;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getNombre(): ?string
+    {
+        return $this->nombre;
+    }
+
+    public function setNombre(string $nombre): self
+    {
+        $this->nombre = $nombre;
+
+        return $this;
+    }
+
+    public function getFechaCreacion(): ?\DateTimeInterface
+    {
+        return $this->fecha_creacion;
+    }
+
+    public function setFechaCreacion(\DateTimeInterface $fecha_creacion): self
+    {
+        $this->fecha_creacion = $fecha_creacion;
+
+        return $this;
+    }
+
+    public function getFechaTope(): ?\DateTimeInterface
+    {
+        return $this->fecha_tope;
+    }
+
+    public function setFechaTope(\DateTimeInterface $fecha_tope): self
+    {
+        $this->fecha_tope = $fecha_tope;
+
+        return $this;
+    }
+
+    public function getEstado(): ?string
+    {
+        return $this->estado;
+    }
+
+    public function setEstado(string $estado): self
+    {
+        $this->estado = $estado;
+
+        return $this;
+    }
+
+    /** 
+    *  @ORM\PrePersist 
+    */
+    public function doStuffOnPrePersist()
+    {
+        $this->fecha_creacion = new \DateTime();
+    }
+}
